7c39a3732da59c12918798bfa21f4c5987a14f16
[Alexis] <[lex.collado@gmail.com]>
2015-11-19 12:06:45 +0800

    Added all files.

---
+++ index.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Kythe Website</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="hover.css">
+	<link rel="stylesheet" href="normalize.css">
+	
+</head>
+<body>
+<!--Start of Container-->
+	<div class="container">
+	<div class="cloud1"></div>
+	<div class="cloud2"></div>
+	<div class="ftk"></div>
+	<div class="events-transition"></div>
+	<div class="literalkites"></div>
+		<!--Start of Wrapper-->
+		<div class="wrapper">
+			<!--Start of Main Content-->
+			<div class="main-content">
+
+				<img class="logo img-responsive" src="img/kythelogo.png" alt="">
+				<img class="kitefly img-responsive" src="img/kitefly.png" alt="">
+				<img class="mainsub img-responsive" src="img/mainsub.png" alt="">
+			</div>
+			
+			<!--Start of Icon Navigation-->
+
+			<nav class="navigation">
+				<ul class="nav-ul2">
+					<a data-scroll href="#aboutus" class="nav-link page-scroll">
+						<div class="link-block2">
+							<li class="nav-item"><img class="icon hvr-bounce-in" src="img/aboutus.png" alt=""></li>
+						</div>
+					</a>
+					<a data-scroll href="#partners" class="nav-link">
+						<div class="link-block2">
+							<li class="nav-item"><img class="icon hvr-bounce-in" src="img/partner.png" alt=""></li>
+						</div>
+					</a>
+					<a data-scroll href="#events" class="nav-link">
+						<div class="link-block2">
+							<li class="nav-item"><img class="icon hvr-bounce-in" src="img/events.png" alt=""></li>
+						</div>
+					</a>
+					<a href="" class="hide nav-link">
+						<img class="logo2 hide" src="img/kythelogo2.png" alt="">
+					</a>
+					<a data-scroll href="#blog" class="nav-link">
+						<div class="link-block2">
+							<li class="nav-item"><img class="icon hvr-bounce-in" src="img/blog.png" alt=""></li>
+						</div>
+					</a>
+					<a data-scroll href="#getinvolved" class="nav-link">
+						<div class="link-block2">
+							<li class="nav-item"><img class="icon hvr-bounce-in" src="img/getinvolved.png" alt=""></li>
+						</div>
+					</a>
+					<a data-scroll href="#contact" class="nav-link">
+						<div class="link-block2">
+							<li class="nav-item"><img class="icon hvr-bounce-in" src="img/contact.png" alt=""></li>
+						</div>
+					</a>
+				</ul>
+			</nav>
+
+			<!--End of Icon Navigation-->
+
+			<!--Start of Sticky Navigation-->
+
+			<nav id="header" class="sticky-navigation">
+				<ul class="nav-ul">
+					<a data-scroll href="#aboutus" class="nav-link">
+						<div class="link-block">
+							<li class="nav-item">ABOUT US</li>
+						</div>
+					</a>
+					<a data-scroll href="#partners" class="nav-link">
+						<div class="link-block">
+							<li class="nav-item">PARTNERS</li>
+						</div>
+					</a>
+					<a data-scroll href="#events" class="nav-link">
+						<div class="link-block">
+							<li class="nav-item">EVENTS</li>
+						</div>
+					</a>
+					<a href="" class="nav-link">
+						<img class="logo2" src="img/kythelogo2.png" alt="">
+					</a>
+					<a data-scroll href="#blog" class="nav-link">
+						<div class="link-block">
+							<li class="nav-item">BLOG</li>
+						</div>
+					</a>
+					<a data-scroll href="#getinvolved" class="nav-link">
+						<div class="link-block">
+							<li class="nav-item">GET INVOLVED</li>
+						</div>
+					</a>
+					<a data-scroll href="#contact" class="nav-link">
+						<div class="link-block">
+							<li class="nav-item">CONTACT US</li>
+						</div>
+					</a>
+				</ul>
+
+			</nav>
+
+			<!--End of Sticky Navigation-->
+			
+
+			<!--Hidden Nav-->
+			
+			<nav id="header2"class="sticky-navigation2">
+				<ul class="nav-ul">
+					<a href="" class="nav-link hidden">
+						<div class="link-block">
+							<li class="nav-item">ABOUT US</li>
+						</div>
+					</a>
+					<a href="" class="nav-link hidden">
+						<div class="link-block">
+							<li class="nav-item">PARTNERS</li>
+						</div>
+					</a>
+					<a href="" class="nav-link hidden">
+						<div class="link-block">
+							<li class="nav-item">EVENTS</li>
+						</div>
+					</a>
+					<a href="" class="nav-link hidden">
+						<img class="logo2" src="img/kythelogo2.png" alt="">
+					</a>
+					<a href="" class="nav-link hidden">
+						<div class="link-block">
+							<li class="nav-item">BLOG</li>
+						</div>
+					</a>
+					<a href="" class="nav-link hidden">
+						<div class="link-block">
+							<li class="nav-item">GET INVOLVED</li>
+						</div>
+					</a>
+					
+						<div class="link-block">
+							<li class="nav-item-important"><strong><a href="" class="nav-link2">LOGIN</a></strong></li>
+						</div>
+				</ul>
+
+			</nav>
+
+
+			<!--End Hidden Nav-->
+
+			<!--End of Main Content-->
+
+			<!--Start of About Us-->
+			<div id="aboutus" class="about-us">
+				<div class="mission-vision">
+					<img class="ribbon" src="img/vision.png" alt="">
+
+					<p>The primary vision of Kythe-Ateneo is to <strong>uplift the spirits</strong> of pediatric patients and their families through <strong>hospital volunteerism.</strong></p>
+
+					<p>Kythe-Ateneo also aims to <strong>promote awareness and advocacy of the Child Life Program</strong> in the Philippines and consequently, <strong>form its members into responsible and compassionate persons.</strong></p>
+
+					<p>In line with this, Kythe-Ateneo has three aspects:</p>
+					
+					<div class="col-flex justify-between vision">
+						<div class="col-3"><img class="vision-pic" src="img/memberformations.png" alt=""><br><p>MEMBER FORMATIONS</p></div>
+						<div class="col-3"><img class="vision-pic" src="img/hospitalvisits.png" alt=""><br><p>HOSPITAL VISITS</p></div>
+						<div class="col-3"><img class="vision-pic" src="img/projectinvolvement.png" alt=""><br><p>PROJECT INVOLVEMENT</p></div>
+					</div>
+					<img class="ribbon" src="img/mission.png" alt="">
+
+					<p>Guided by Kythe-Ateneo’s vision, the organization’s primary mission is <strong>to provide direct (psychosocial) and indirect (financial) assistance</strong> to pediatric patients with chronic illnesses and their families in AFP Medical Center (AFPMC), Philippine Orthopedic Center (POC), Quirino Memorial Medical Center (QMMC), Philippine Children's Medical Center (PCMC), Philippine Heart Center (PHC), and National Children’s Hospital (NCH); and in so doing, form its members into responsible and compassionate persons.</p>
+
+
+					<p>Psycho-social support will be given in the form of prepared <strong>structured activities</strong> for the pediatric patients, including both bedside and playroom activities held in the hospital. Indirect assistance will be given in the form of money or goods which must be channeled through Kythe Foundation, Inc.</p>
+
+
+					<p>Kythe’s secondary mission is <strong>to promote awareness of what the Kythe Child Life Program is</strong> through on and off campus advocacy and finance activities.</p>
+
+				</div>
+				
+				<div class="childlife-program">
+					<h1 class="stylish-h1">The Child Life Program</h1>
+					<p>The objective of the Kythe Child Life Program is <strong>to provide psycho-social support that alleviates the anxiety of pediatric patients who suffer from illnesses</strong> such as cancer, heart condition, kidney disease, and blood disorders. The Kythe Child Life Program specializes in <strong>promoting optimum growth, fostering development and helping the children cope with the challenge of hospitalization</strong></p>
+				</div>
+
+				<img class="transition1" src="img/transition1.png" alt="">
+				
+
+			</div>
+			<!--End of About Us-->
+			<!--Start of Partners-->
+			<div id="partners" class="partners">
+				<h1 class="h1-partners">PARTNERS</h1>
+				<img class="hospitals" src="img/hospitals.png" alt="">
+				<div class="col-flex justify-between vision">
+						<div class="col-3"><img class="vision-pic" src="img/hospital1.png" alt=""><br><p>AFP MEDICAL CENTER</p><br>
+						<ul class="social">
+							<li><a href=""><img class="hvr-grow small-icon" src="img/facebook.png" alt=""></a></li>
+							<li><a href=""><img class="hvr-grow small-icon" src="img/location.png" alt=""></a></li>
+						</ul>
+						</div>
+						<div class="col-3"><img class="vision-pic" src="img/hospital2.png" alt=""><br><p>PHILIPPINE ORTHOPEDIC CENTER</p><br>
+						<ul class="social">
+							<li><a href=""><img class="hvr-grow small-icon" src="img/facebook.png" alt=""></a></li>
+							<li><a href=""><img class="hvr-grow small-icon" src="img/location.png" alt=""></a></li>
+						</ul></div>
+						<div class="col-3"><img class="vision-pic" src="img/hospital3.png" alt=""><br><p>PHILIPPINE CHILDREN'S CENTER</p><br>
+						<ul class="social">
+							<li><a href=""><img class="hvr-grow small-icon" src="img/facebook.png" alt=""></a></li>
+							<li><a href=""><img class="hvr-grow small-icon" src="img/location.png" alt=""></a></li>
+						</ul></div>
+				</div>
+				<div class="col-flex justify-between vision">
+						<div class="col-3"><img class="vision-pic" src="img/hospital4.png" alt=""><br><p>PHILIPPINE HEART CENTER</p><br>
+						<ul class="social">
+							<li><a href=""><img class="hvr-grow small-icon" src="img/facebook.png" alt=""></a></li>
+							<li><a href=""><img class="hvr-grow small-icon" src="img/location.png" alt=""></a></li>
+						</ul></div>
+						<div class="col-3"><img class="vision-pic" src="img/hospital5.png" alt=""><br><p>PHILIPPINE CHILDREN'S HOSPITAL</p><br>
+						<ul class="social">
+							<li><a href=""><img class="hvr-grow small-icon" src="img/facebook.png" alt=""></a></li>
+							<li><a href=""><img class="hvr-grow small-icon" src="img/location.png" alt=""></a></li>
+						</ul></div>
+						<div class="col-3"><img class="vision-pic" src="img/hospital6.png" alt=""><br><p>PHILIPPINE HEART CENTER</p><br>
+						<ul class="social">
+							<li><a href=""><img class="hvr-grow small-icon" src="img/facebook.png" alt=""></a></li>
+							<li><a href=""><img class="hvr-grow small-icon" src="img/location.png" alt=""></a></li>
+						</ul></div>
+				</div>
+
+				<img class="foundation" src="img/foundation.png" alt="">
+				<p class="desc">Kythe-Ateneo is the primary student-arm of Kythe Foundation, Inc., a non-profit, non-stock organization aimed towards <strong>improving the quality of life among hospitalized children with cancer and other chronic illnesses</strong. To get to know more about our mother organization, visit them at <strong>www.kythe.org</strong></p>
+			</div>
+			<!--End of Partners-->	
+
+			<!--Start of Events-->
+			<div id="events" class="events">
+				<h1 class="h1-events">EVENTS</h1>
+				<div class="event-images">
+					<div class="col-2">
+						<img src="img/event-reflectionsessions.png" alt="">
+						<img src="img/event-hospitalvisits.png" alt="">
+						<img src="img/event-iamhope.png" alt="">
+						<img src="img/event-chronicillnessweek.png" alt="">
+						<img src="img/event-kytrip.png" alt="">
+					</div>
+					<div class="col-2">
+						<img src="img/event-celebratelifeparty.png" alt="">
+						<img src="img/event-memberformation.png" alt="">
+						<img src="img/event-kythekids.png" alt="">
+						<img src="img/event-financeprojects.png" alt="">
+						<img src="img/event-kytheweek.png" alt="">
+					</div>					
+				</div>
+			</div>
+
+			<!--End of Events-->
+
+			<!--Start of Blog-->
+			<div class="blog">
+				<h1 class="h1-blog">BLOG</h1>
+			</div>
+			
+			<div id="blog" class="blog-wrapper">
+				<div  class="main-article">
+					<div class="main-blog-image">
+						<img class="main-blog-image" src="img/sample.jpg" alt="">
+					</div>
+					<a href=""><h1>This is the title of the blog page.</h1></a>
+					<p class="main-authordate">Written by Alexis Collado on July 2, 2015</p>
+					<p class="main-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit culpa, ratione voluptatum quo enim voluptatibus rem. Minima cum consequuntur in vero, autem iusto voluptate aut asperiores. Voluptatem labore dignissimos veniam.</p>
+				</div>
+				
+				<div class="main-article">
+					<div class="main-blog-image">
+						<img class="main-blog-image" src="img/sample.jpg" alt="">
+					</div>
+					<a href=""><h1>This is the title of the blog page.</h1></a>
+					<p class="main-authordate">Written by Alexis Collado on July 2, 2015</p>
+					<p class="main-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit culpa, ratione voluptatum quo enim voluptatibus rem. Minima cum consequuntur in vero, autem iusto voluptate aut asperiores. Voluptatem labore dignissimos veniam.</p>
+				</div>
+
+
+
+
+			</div>
+
+			<div class="wrap">
+				<a class="pink-button" href="#"><div>See all articles</div></a>
+			</div>
+
+			
+
+
+
+
+
+
+			<!--End of Blog-->
+
+
+
+			<!--Start of Get Involved-->
+
+			<div id="getinvolved" class="getinvolved">
+				<img class="happypeople" src="img/happypeople.png" alt="">
+					<h1 class="h1-getinvolved">GET INVOVED</h1>
+					<div class="new-wrapper">
+					<h2>Be a Volunteer!</h2>
+					<h3>BE A BUDDY!</h3>
+					<p>Sign up to be a buddy in our advocacy events! The sign up link will be released a few weeks before the event. Keep yourself posted and visit our Facebook page for more details.</p>
+					<h3>BE A BOOTH SPONSOR!</h3>
+					<p>Take part in our advocacy events by being a booth sponsor in our advocacy events! Set up a game or activity booth where the kids can play and learn.</p>
+					<h2>Donate!</h2>
+					<p>Hair2Share is a hair donation effort by the organization, which aims to collect hair to be made in to clinical wigs for patients with cancer. Simply drop by in any of our partner salons any time until February 2016 and have your hair cut! </p>
+					<h2>Be a Sponsor!</h2>	
+					<p>Support our advocacy by sponsoring Kythe-Ateneo and our events. Contact us at kythe.ateneo@yahoo.com
+
+to know more about how you can help.</p>
+				</div>
+			</div>
+			
+
+			<!--End of Get Involved-->
+
+			<!--Start of Contact Us-->
+
+			<div id="contact" class="contactus">
+				<h1 class="h1-contactus">CONTACT US</h1>
+			</div>
+			
+
+			<!--End of Contact Us-->
+
+
+
+		</div>
+		<!--End of Wrapper-->
+	</div>
+<!--End of Container-->
+
+<script src="smooth-scroll.js"></script>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <script type="text/javascript" src="scroll.js"></script>
+  <script type="text/javascript" src="jquery.sticky.js"></script>
+  <script>
+    $(window).load(function(){
+      $("#header").sticky({ topSpacing: 0 });
+    });
+
+    
+    $(window).load(function(){
+      $("#header2").sticky({ topSpacing: 50 });
+    });  
+  </script>
+
+
+<script>
+    smoothScroll.init();
+</script>
+
+	<script src="SmoothScroll.js"></script>
+
+</body>
+</html>
+
